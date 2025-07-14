65c8e1cccb7c01e92fbf3bd381e3af5949702f21
harshwardhan.joundal <harshwardhan.joundal@pw.live>
2025-07-14 22:21:34 +0530

    eks and vpc

--- main.tf
+++ main.tf
@@ -26,24 +26,24 @@ module "jenkins_ec2" {
   source = "./modules/ec2_jenkins"
 }
 
-# module "vpc" {
-#   source = "./modules/vpc"
+module "vpc" {
+  source = "./modules/vpc"
 
-#   vpc_cidr = var.vpc_cidr
-#   public_subnet_cidrs = var.public_subnet_cidrs
-#   private_subnet_cidrs = var.private_subnet_cidrs
-#   cluster_name = var.cluster_name
-#   availability_zones = var.availability_zones
+  vpc_cidr = var.vpc_cidr
+  public_subnet_cidrs = var.public_subnet_cidrs
+  private_subnet_cidrs = var.private_subnet_cidrs
+  cluster_name = var.cluster_name
+  availability_zones = var.availability_zones
 
-# }
+}
 
 
-# module "eks" {
-#   source = "./modules/eks"
+module "eks" {
+  source = "./modules/eks"
 
-#   cluster_name    = var.cluster_name
-#   cluster_version = var.cluster_version
-#   vpc_id          = module.vpc.vpc_id
-#   subnet_ids      = module.vpc.private_subnet_ids
-#   node_groups     = var.node_groups
-# }
+  cluster_name    = var.cluster_name
+  cluster_version = var.cluster_version
+  vpc_id          = module.vpc.vpc_id
+  subnet_ids      = module.vpc.private_subnet_ids
+  node_groups     = var.node_groups
+}
